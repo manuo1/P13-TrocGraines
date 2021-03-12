f6cb45c38ea61707bda87dbd0697f88fd56b3634
manuo1 <manuo1@free.fr>
2021-03-12 13:55:34 +0100

    setting up the project base

---
+++ trocgraines/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TrocgrainesConfig(AppConfig):
+    name = 'trocgraines'
---
+++ trocgraines_config/wsgi.py
@@ -0,0 +1,23 @@
+"""
+WSGI config for trocgraines project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from dotenv import find_dotenv, load_dotenv
+
+""" this part will load environment variable and designate the """
+""" right settings file to use depending on the environment"""
+load_dotenv(find_dotenv())
+environment = os.environ['ENVIRONMENT']
+os.environ.setdefault(
+    'DJANGO_SETTINGS_MODULE', 'trocgraines_config.settings.{}'.format(environment)
+)
+
+application = get_wsgi_application()

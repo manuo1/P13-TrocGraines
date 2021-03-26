d3e69c2549dbc64d12cdafe6cc728bc17e73d777
manuo1 <manuo1@free.fr>
2021-03-26 11:40:25 +0100

    change the base resolution of downloaded media images

--- seeds/models.py
+++ seeds/models.py
@@ -59,7 +59,7 @@ class Seed(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     photo = ProcessedImageField(upload_to='images/',
-                                   processors=[ResizeToFill(800, 600)],
+                                   processors=[ResizeToFill(640, 360)],
                                    format='JPEG',
                                    options={'quality': 60})
     creation_date = models.DateTimeField(auto_now_add=True)
